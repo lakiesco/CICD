d3cc83000721ffc6ad564ddd54d185c775cb52ef
lakiesco <dima271199@yandex.ru>
2025-01-29 18:35:00 +0300

    fix tg.sh

--- src/tg.sh
+++ src/tg.sh
@@ -1,24 +1,31 @@
-variables:
-  TELEGRAM_API: "https://api.telegram.org/bot${7456794509:AAGWuVhA_1odOJYw4sXwH6IkPj8GzX2r8rY}/sendMessage"
+#!/bin/bash
 
-send_telegram_message:
-  script:
-    - |
-      STATUS="✅ Успешно"
-      if [ "$CI_JOB_STATUS" != "success" ]; then
-        STATUS="❌ Ошибка"
-      fi
-      MESSAGE="🔔 *CI/CD Notification* 🔔
-      
-      📌 Проект: *${CI_PROJECT_NAME}*
-      🔄 Ветка: *${CI_COMMIT_REF_NAME}*
-      🚀 Pipeline ID: *${CI_PIPELINE_ID}*
-      🔍 Job: *${CI_JOB_NAME}*
-      💡 Статус: *${STATUS}*"
+# НИКОГДА НЕ ХРАНИТЬ ТАК ДАННЫЕ!!!
+TELEGRAM_BOT_TOKEN=7456794509:AAGWuVhA_1odOJYw4sXwH6IkPj8GzX2r8rY
+TELEGRAM_CHAT_ID=419527685
 
-      curl -s -X POST "${TELEGRAM_API}" \
-        -d chat_id="${419527685}" \
-        -d text="${MESSAGE}" \
-        -d parse_mode="Markdown"
-  after_script:
-    - send_telegram_message
+# Проверка наличия переменных
+if [[ -z "$TELEGRAM_BOT_TOKEN" || -z "$TELEGRAM_CHAT_ID" ]]; then
+  echo "Ошибка: TELEGRAM_BOT_TOKEN или TELEGRAM_CHAT_ID не заданы!"
+  exit 1
+fi
+
+# Определение статуса
+STATUS="✅ Успешно"
+if [[ "$CI_JOB_STATUS" != "success" ]]; then
+  STATUS="❌ Ошибка"
+fi
+
+# Формирование сообщения
+MESSAGE="🔔 *CI/CD Notification*  
+📌 Проект: *${CI_PROJECT_NAME}*  
+🔄 Ветка: *${CI_COMMIT_REF_NAME}*  
+🔧 Джоб: *${CI_JOB_NAME}*  
+💡 Статус: *${STATUS}*"
+
+# Отправка уведомления в Telegram
+curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
+  -d chat_id="${TELEGRAM_CHAT_ID}" \
+  -d text="${MESSAGE}" \
+  -d parse_mode="Markdown"
+
