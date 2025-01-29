a45fdbbc637861de4573a6b5ce895bf952fc50c0
lakiesco <dima271199@yandex.ru>
2025-01-29 19:24:59 +0300

    fix bugs

--- src/tg.sh
+++ src/tg.sh
@@ -4,19 +4,6 @@
 TELEGRAM_BOT_TOKEN="7456794509:AAGWuVhA_1odOJYw4sXwH6IkPj8GzX2r8rY"
 TELEGRAM_CHAT_ID="419527685"
 
-# Проверка наличия переменных
-if [[ -z "$TELEGRAM_BOT_TOKEN" || -z "$TELEGRAM_CHAT_ID" ]]; then
-  echo "Ошибка: TELEGRAM_BOT_TOKEN или TELEGRAM_CHAT_ID не заданы!"
-  exit 1
-fi
-
-# Определение статуса
-if [[ "$CI_JOB_STATUS" == "success" && $? -eq 0 ]]; then
-  STATUS="✅ Успешно"
-else
-  STATUS="❌ Ошибка"
-fi
-
 # Формирование сообщения
 MESSAGE="🔔 *CI/CD Notification*  
 📌 Проект: *${CI_PROJECT_NAME}*  
